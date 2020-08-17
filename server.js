8bc0cfe8070fb105372094bf76f1790d9b83db17
Junaid <junaid@Cayzens-MacBook-Air.local>
2020-08-17 14:24:33 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env'})
 
@@ -13,6 +14,14 @@ const users = require('./routes/users');
 const app = express();
 app.use(express.json());
 app.use('/api/v1/users',users);
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
