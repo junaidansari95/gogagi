fa13a0f105cf1e1933f2a1b5ed89592273b78617
Junaid <junaid@Cayzens-MacBook-Air.local>
2020-08-17 14:35:22 +0530

    version 1.1.0

--- client/src/Actions/userAction.js
+++ client/src/Actions/userAction.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 export const getUsers = () => dispatch => {
   dispatch({ type: 'GET_ALL_USERS_REQUEST' })
-  axios.get('http://localhost:3000//api/v1/users/')
+  axios.get('/api/v1/users/')
     .then(result => {
       dispatch({ type: 'GET_ALL_USERS_SUCCESS', payload: result.data.data })
     })
     .catch(err => {
-      dispatch({ type: 'GET_ALL_USERS_FAILURE', payload: err.response.data })
+      dispatch({ type: 'GET_ALL_USERS_FAILURE', payload: err.response })
     });
 };
 export const addUser = (data) => dispatch => {
   dispatch({ type: 'ADD_NEW_USER_REQUEST' })
-  axios.post('http://localhost:3000//api/v1/users/', data)
+  axios.post('/api/v1/users/', data)
     .then(result => {
       dispatch({ type: 'ADD_NEW_USER_SUCCESS', payload: result.data.data })
       dispatch(getUsers())
     })
     .catch(err => {
-      dispatch({ type: 'ADD_NEW_USER_FAILURE', payload: err.response.data })
+      dispatch({ type: 'ADD_NEW_USER_FAILURE', payload: err.response })
     });
 };
